df84d9353aa1dbe249f3b00949dfac082c57a7da
Aslan <thebestdevelopering@gmail.com>
2022-02-17 18:13:12 +0300

    create todolist project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig(
+  {
+    transpileDependencies: true,
+  },
+  {
+    css: {
+      loaderOptions: {
+        sass: {
+          prepenData: ' @import "@/assets/styles.sass"; ',
+        },
+      },
+    },
+  }
+);
+
+// module.exports = {
+//   css: {
+//     loaderOptions: {
+//       sass: {
+//         prepenData: ' @import "@/assets/styles.sass"; '
+//       }
+//     }
+//   }
+// }
