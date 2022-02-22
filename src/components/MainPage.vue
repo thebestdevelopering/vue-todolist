dc3cd3b1d6bf50d3052425138dd51ac966e141e8
Aslan <thebestdevelopering@gmail.com>
2022-02-22 11:34:12 +0300

    create component checkbox

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,35 +2,35 @@
   <div class="container">
     <div class="todolist">
       <div class="header">
-          <p class="header__text_title">Список задач</p>
+        <p class="header__text_title">Список задач</p>
       </div>
       <div class="main">
         <div class="main--tasks">
-          <p class="main--tasks__checkbox"></p>
+          <p class="main__checkbox main__checkbox_disabled"></p>
           <input
             class="main--tasks_text"
             type="text"
             placeholder="Введите текст задачи..."
           />
           <div class="main--tasks_add">
-            <img src="../assets/images/Plus.svg" alt="">
+            <img src="../assets/images/Plus.svg" alt="" />
           </div>
         </div>
 
         <div class="main--tasks">
-          <p class="main--tasks__checkbox work"></p>
+          <p class="main__checkbox main__checkbox_active"></p>
           <p class="main--tasks__text">Изучить ReactJS</p>
           <div class="main--tasks__actions">
             <div class="main--tasks__actions_block">
               <img
-                class="main--task__img"
+                class="main--task_edit"
                 src="../assets/images/Edit.svg"
                 alt=""
               />
             </div>
             <div class="main--tasks__actions_block">
               <img
-                class="main--task__img"
+                class="main--task_delete"
                 src="../assets/images/Cart.svg"
                 alt=""
               />
@@ -38,25 +38,26 @@
           </div>
         </div>
         <div class="main--tasks">
-          <p class="main--tasks__checkbox active">
+          <Radio checked />
+          <!-- <p class="main__checkbox checked">
             <img
               class="main--task__check"
               src="../assets/images/Vector.svg"
               alt=""
             />
-          </p>
+          </p> -->
           <p class="main--tasks__text">Создать TODO</p>
           <div class="main--tasks__actions">
             <div class="main--tasks__actions_block">
               <img
-                class="main--task__img"
+                class="main--task_edit"
                 src="../assets/images/Edit.svg"
                 alt=""
               />
             </div>
             <div class="main--tasks__actions_block">
               <img
-                class="main--task__img"
+                class="main--task_delete"
                 src="../assets/images/Cart.svg"
                 alt=""
               />
@@ -68,6 +69,7 @@
         <div class="footer--btn">
           <div class="footer--btn__actions mark">
             <img
+              width="16"
               class="footer--btn__img"
               src="../assets/images/All.svg"
               alt=""
@@ -76,6 +78,7 @@
           </div>
           <div class="footer--btn__actions delete">
             <img
+              width="11"
               class="footer--btn__img"
               src="../assets/images/Delete.svg"
               alt=""
@@ -87,3 +90,13 @@
     </div>
   </div>
 </template>
+
+<script>
+import Radio from './radio-box.vue';
+
+export default {
+  components: {
+    Radio,
+  },
+};
+</script>
---
+++ src/components/radio-box.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="main__checkbox" :class="{ checked: isActive }">
+    <img class="main--task__check" src="../assets/images/Vector.svg" alt="" />
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: Boolean,
+    checked: Boolean,
+  },
+  data() {
+    return {
+      isActive: true,
+      hasError: false,
+    };
+  },
+};
+</script>
