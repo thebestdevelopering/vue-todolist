a55cf0725d6d29bd6fbf96797e572af6943cf0c3
Aslan <thebestdevelopering@gmail.com>
2022-02-23 13:49:18 +0300

    getting data from an array

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -17,30 +17,9 @@
           </div>
         </div>
 
-        <div class="main--tasks" v-for="task in taskActive" :key="task">
-          <radio-box></radio-box>
-          <p class="main--tasks__text">
-            {{ task.note }}
-          </p>
-          <div class="main--tasks__actions">
-            <div class="main--tasks__actions_block">
-              <img
-                class="main--task_edit"
-                src="../assets/images/Edit.svg"
-                alt=""
-              />
-            </div>
-            <div class="main--tasks__actions_block">
-              <img
-                class="main--task_delete"
-                src="../assets/images/Cart.svg"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-        <div class="main--tasks" v-for="task in taskChecked" :key="task">
-          <radio-box checked> </radio-box>
+        <div class="main--tasks" v-for="task in tasks" :key="task">
+          <radio-box v-if="task.checked" checked></radio-box>
+          <radio-box v-else></radio-box>
           <p class="main--tasks__text">
             {{ task.note }}
           </p>
@@ -97,7 +76,7 @@ export default {
       tasks: [
         { note: 'Изучить Vue', checked: true },
         { note: 'Проснуться в 7 утра' },
-        { note: 'Сделать стрижку', checked: false },
+        { note: 'Сделать стрижку' },
         { note: 'Сделать прививку', checked: true },
       ],
     };
