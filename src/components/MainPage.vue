1fa9bb3bbbe719cf81580bfbba56ac64e01f3e34
Aslan <thebestdevelopering@gmail.com>
2022-02-23 17:28:35 +0300

    create list item component

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,39 +5,7 @@
         <p class="header__text_title">Список задач</p>
       </div>
       <div class="main">
-        <div class="main--tasks">
-          <RadioBox disabled />
-          <input
-            class="main--tasks_text"
-            type="text"
-            placeholder="Введите текст задачи..."
-          />
-          <div class="main--tasks_add">
-            <img src="../assets/images/Plus.svg" alt="" />
-          </div>
-        </div>
-        <div class="main--tasks" v-for="task in tasks" :key="task">
-          <radio-box :checked="task.checked"></radio-box>
-          <p class="main--tasks__text">
-            {{ task.note }}
-          </p>
-          <div class="main--tasks__actions">
-            <div class="main--tasks__actions_block">
-              <img
-                class="main--task_edit"
-                src="../assets/images/Edit.svg"
-                alt=""
-              />
-            </div>
-            <div class="main--tasks__actions_block">
-              <img
-                class="main--task_delete"
-                src="../assets/images/Cart.svg"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
+        <ListItem />
       </div>
       <div class="footer">
         <div class="footer--btn">
@@ -66,21 +34,11 @@
 </template>
 
 <script>
-import RadioBox from './radio-box.vue';
+import ListItem from './list-item.vue';
 
 export default {
-  data() {
-    return {
-      tasks: [
-        { note: 'Изучить Vue', checked: true },
-        { note: 'Проснуться в 7 утра' },
-        { note: 'Сделать стрижку' },
-        { note: 'Сделать прививку', checked: true },
-      ],
-    };
-  },
   components: {
-    RadioBox,
+    ListItem,
   },
 };
 </script>
---
+++ src/components/list-item.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="main--tasks">
+    <RadioBox disabled />
+    <input
+      class="main--tasks_text"
+      type="text"
+      placeholder="Введите текст задачи..."
+    />
+    <div class="main--tasks_add">
+      <img src="../assets/images/Plus.svg" alt="" />
+    </div>
+  </div>
+  <div class="main--tasks" v-for="task in tasks" :key="task">
+    <radio-box :checked="task.checked" />
+    <p class="main--tasks__text">
+      {{ task.note }}
+    </p>
+    <div class="main--tasks__actions">
+      <div class="main--tasks__actions_block">
+        <img class="main--task_edit" src="../assets/images/Edit.svg" alt="" />
+      </div>
+      <div class="main--tasks__actions_block">
+        <img class="main--task_delete" src="../assets/images/Cart.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RadioBox from './radio-box.vue';
+
+export default {
+  data() {
+    return {
+      tasks: [
+        { note: 'Изучить Vue', checked: true },
+        { note: 'Проснуться в 7 утра' },
+        { note: 'Сделать стрижку' },
+        { note: 'Сделать прививку', checked: true },
+      ],
+    };
+  },
+  components: {
+    RadioBox,
+  },
+};
+</script>
